7842392ab0885c8178f36856b7aa7dcfbd4b52d5
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2025-12-01 23:58:01 +0530

    create blog in db diff org

--- app/org/[slug]/actions.ts
+++ app/org/[slug]/actions.ts
@@ -5,6 +5,8 @@ import { blogTable, CreateBlogType } from "@/db/schema";
 
 //server function to create blog
 export const createBlog = async(payload: CreateBlogType) => {
-  const result = await db.insert(blogTable).values(payload)
+  const result = await db.insert(blogTable).values(payload).returning({
+    id: blogTable.id,
+  });
     return result;
 }
